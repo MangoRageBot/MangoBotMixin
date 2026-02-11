5ee6a2f57ce8c1c5a3a7904b4bba463f8149fbc2
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2026-02-11 08:51:43 -0800

    Added some more debug guards, so we dont spam console...

--- src/main/java/org/mangorage/mangobotmixin/plugin/MangoBotMixinPlugin.java
+++ src/main/java/org/mangorage/mangobotmixin/plugin/MangoBotMixinPlugin.java
@@ -1,8 +1,10 @@
 package org.mangorage.mangobotmixin.plugin;
 
 
+import org.mangorage.bootstrap.api.loader.IMangoLoader;
 import org.mangorage.mangobotcore.api.plugin.v1.MangoBotPlugin;
 import org.mangorage.mangobotcore.api.plugin.v1.Plugin;
+import org.objectweb.asm.ClassReader;
 
 @MangoBotPlugin(id = MangoBotMixinPlugin.ID)
 public final class MangoBotMixinPlugin implements Plugin {
@@ -16,5 +18,17 @@ public final class MangoBotMixinPlugin implements Plugin {
     @Override
     public void load() {
         System.out.println("Loaded MangoBotMixinPlugin");
+        IMangoLoader mangoLoader = (IMangoLoader) this.getClass().getClassLoader();
+
+        final var history = mangoLoader.getTransformerHistory().getHistory("org.mangorage.mangobotcore.internal.ExampleThing");
+        final var result = history.get(1);
+
+        System.out.println("Transformed class bytes:");
+        ClassReader cr = new ClassReader(result.transformerResult());
+        cr.accept(new org.objectweb.asm.util.TraceClassVisitor(new java.io.PrintWriter(System.out)), 0);
+
+        System.out.println("Original class bytes:");
+        ClassReader cr2 = new ClassReader(result.classData());
+        cr2.accept(new org.objectweb.asm.util.TraceClassVisitor(new java.io.PrintWriter(System.out)), 0);
     }
 }
